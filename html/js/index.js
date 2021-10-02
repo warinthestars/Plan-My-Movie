5e0b5e463eaf6eec2eb9d10503ce954fb80b8755
stonefrango <43227653+stonefrango@users.noreply.github.com>
2021-10-01 22:22:36 -0400

    cleaned up some unnecessary tab space

--- html/js/index.js
+++ html/js/index.js
@@ -15,18 +15,18 @@ $(function() {
 		var $idforgetdetails = $value.text(JSON.stringify($input.val()));
 		function ajaxget(){
 			var jsonresult = $.ajax({
-								url: 'https://api.themoviedb.org/3/movie/' + $input.val() + '?api_key=3634a680b1c4dff46bdba60a88625d55&language=en-US',
-								type: 'GET',
-								dataType: 'jsonp',
-								error: function() {
-									console();
-								},
-								success: function(detailsresponse) {
-									console.log(detailsresponse);
-									document.getElementById("displayrt").innerHTML = 'Run-Time: ' + detailsresponse.runtime;
-									console.log("rtset: " + detailsresponse.runtime);
-								}
-							});
+				url: 'https://api.themoviedb.org/3/movie/' + $input.val() + '?api_key=3634a680b1c4dff46bdba60a88625d55&language=en-US',
+				type: 'GET',
+				dataType: 'jsonp',
+				error: function() {
+					console();
+				},
+				success: function(detailsresponse) {
+					console.log(detailsresponse);
+					document.getElementById("displayrt").innerHTML = 'Run-Time: ' + detailsresponse.runtime;
+					console.log("rtset: " + detailsresponse.runtime);
+				}
+			});
 		}
 	});
 });
