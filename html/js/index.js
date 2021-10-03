40bf490fef01bba42e6cb657c161a8c4f013cd4a
stonefrango <43227653+stonefrango@users.noreply.github.com>
2021-10-02 22:12:27 -0400

    working prototype for if movie starts right now

--- html/js/index.js
+++ html/js/index.js
@@ -13,6 +13,8 @@ $(function() {
 		$idcontainer.insertAfter($input);
 		
 		var $idforgetdetails = $value.text(JSON.stringify($input.val()));
+		var currenttime;
+		var movieendtime;
 		function ajaxget(){
 			var jsonresult = $.ajax({
 				url: 'https://api.themoviedb.org/3/movie/' + $input.val() + '?api_key=3634a680b1c4dff46bdba60a88625d55&language=en-US',
@@ -22,11 +24,32 @@ $(function() {
 					console();
 				},
 				success: function(detailsresponse) {
+					var movieruntime = detailsresponse.runtime;
 					console.log(detailsresponse);
-					document.getElementById("displayrt").innerHTML = 'Run-Time: ' + detailsresponse.runtime;
-					console.log("rtset: " + detailsresponse.runtime);
+					document.getElementById("displayrt").innerHTML = 'Movie Run-Time: ' + timeConvert(movieruntime);
+					console.log("movie run time: " + movieruntime);
+					document.getElementById("localtime").innerHTML = 'Current Time: ' + getLocaltime();
+					console.log("current time: " + getLocaltime());
+					document.getElementById("calctime").innerHTML = 'Calculated Movie End Time: ' + addMinutes(currenttime, movieruntime).toLocaleString();
 				}
 			});
+		}
+		
+		var timeConvert = function(n){
+			var minutes = n%60;
+			var hours = (n - minutes) / 60;
+			return hours + " hour(s) and " + minutes + " minute(s)";
+		}
+		
+		function getLocaltime(){
+			currenttime = new Date();
+			return currenttime.toLocaleString();
+		}
+		
+		function addMinutes(date, minutes) {
+			movieendtime = new Date(+date + minutes*60000);
+			console.log("movie end time: " + movieendtime.toLocaleString());
+			return movieendtime;
 		}
 	});
 });
