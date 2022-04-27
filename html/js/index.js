7b6ef5b7feccfda33019ce90c69a7aeaa126af3b
stonefrango <43227653+stonefrango@users.noreply.github.com>
2022-04-26 23:56:22 -0400

    Added "copy movie to clipboard" button

--- html/js/index.js
+++ html/js/index.js
@@ -1,41 +1,16 @@
 var currenttime;
 var movieendtime;
 var movieruntime;
+var currentmovieid = 683970; // hehe
 
-/* function timeCheck() {
-if (document.getElementById('currenttime').checked) {
-	
-	document.getElementById('selectcustomtime').style.display = 'none';
-	document.getElementById('displayrt').style.display = 'block';
-	document.getElementById('custcalctime').style.display = 'none';
-	document.getElementById("calctime").innerHTML = 'Calculated Movie End Time: ' + '<br><br>';
-	document.getElementById('select-movie').value = '';
-	document.getElementById('buttonregcalc').style.display = 'block';
-	document.getElementById('buttoncustcalc').style.display = 'none';
-	console.log("current date and time: " + getLocaltime());
-	console.log("selected movie runtime: " + movieruntime);
-	
-} else {
-	document.getElementById('displayrt').style.display = 'block';
-	document.getElementById('custcalctime').style.display = 'block';
-	document.getElementById('selectcustomtime').style.display = 'block';
-	document.getElementById('timepickergo').value = '';
-	document.getElementById('custcalctime').innerHTML = 'Calculated Movie End Time:' + '<br><br>';
-	document.getElementById('select-movie').value = '';
-	document.getElementById('buttonregcalc').style.display = 'none';
-	document.getElementById('buttoncustcalc').style.display = 'block';
-	
-}} */
-
-$(function() {
+$(function () {
 	document.getElementById("timepickergo").value = getLocaltime();
-}
-)
+})
 
-$(function() {
-	
+$(function () {
+
 	var idforgetdetails;
-	
+
 	$('#select-movie').selectize({
 		valueField: 'id',
 		labelField: 'title',
@@ -43,26 +18,26 @@ $(function() {
 		options: [],
 		create: false,
 		render: {
-			option: function(item, escape) {
-			
-			var tmdbthumb;
-			
-			if (item.poster_path === null){
-				tmdbthumb = "assets/images/noart.png";
-			}else{
-				tmdbthumb = "https://image.tmdb.org/t/p/w92" + item.poster_path;
-			}
-			
+			option: function (item, escape) {
+
+				var tmdbthumb;
+
+				if (item.poster_path === null) {
+					tmdbthumb = "assets/images/noart.png";
+				} else {
+					tmdbthumb = "https://image.tmdb.org/t/p/w92" + item.poster_path;
+				}
+
 				return '<div>' +
 					'<img src="' + escape(tmdbthumb) + '" alt="">' +
 					'<span class="title">' +
-						'<span class="name">' + escape(item.title + " " + "(" + item.release_date + ")") + '</span>' +
+					'<span class="name">' + escape(item.title + " " + "(" + item.release_date + ")") + '</span>' +
 					'</span>' +
 					'<span class="description">' + escape(item.overview || 'No synopsis available at this time.') + '</span>'
 				'</div>';
 			}
 		},
-		load: function(query, callback) {
+		load: function (query, callback) {
 			if (!query.length) return callback();
 			$.ajax({
 				url: 'https://api.planmymovie.com/3/search/movie/',
@@ -71,10 +46,10 @@ $(function() {
 				data: {
 					query: query,
 				},
-				error: function() {
+				error: function () {
 					callback();
 				},
-				success: function(res) {
+				success: function (res) {
 					console.log(res.results);
 					callback(res.results);
 				}
@@ -83,95 +58,96 @@ $(function() {
 	})
 })
 
-$(function() {
-	
+$(function () {
+
 	var $wrapper = document.getElementById('#wrapper');
 	// show current input values
-	$('select.selectized,input.selectized', $wrapper).each(function() {
+	$('select.selectized,input.selectized', $wrapper).each(function () {
 		console.log("Selectize Initialized")
 		var $idcontainer = $('<div style="display:none">').addClass('value').html('Current Value: ');
-		var $value = $('<span>').appendTo($idcontainer); 
+		var $value = $('<span>').appendTo($idcontainer);
 		var $input = $(this);
-		var update = function(e) { $value.text(JSON.stringify($input.val())); }
+		var update = function (e) { $value.text(JSON.stringify($input.val())); }
 		$(this).on('change', update);
 		update();
 		$(this).on('change', ajaxget);
 
 		$idcontainer.insertAfter($input);
-		
+
 		var $idforgetdetails = $value.text(JSON.stringify($input.val()));
 
-		function ajaxget(){
+		function ajaxget() {
 			var jsonresult = $.ajax({
 				url: 'https://api.planmymovie.com/3/movie/?id=' + $input.val(),
 				type: 'GET',
 				dataType: 'json',
-				error: function() {
+				error: function () {
 					console();
 				},
-				success: function(detailsresponse) {
+				success: function (detailsresponse) {
 					
+					currentmovieid = detailsresponse.id;
 					movietitle = detailsresponse.title;
 					document.getElementById("selectedmoviename").innerHTML = movietitle;
 					movieruntime = detailsresponse.runtime;
-					if (detailsresponse.poster_path === null){
+					if (detailsresponse.poster_path === null) {
 						posterpath = "assets/images/noart250w.png";
-					}else{
+					} else {
 						posterpath = "https://image.tmdb.org/t/p/w500" + detailsresponse.poster_path;
 					}
-					document.getElementById("selectedmovieposterimg").src=posterpath;
+					document.getElementById("selectedmovieposterimg").src = posterpath;
 					releasedate = detailsresponse.release_date;
 					document.getElementById("selectedmoviedate").innerHTML = releasedate;
 					synoposis = detailsresponse.overview;
 					document.getElementById("selectedmoviesynopsis").innerHTML = synoposis;
-					
+
 					console.log(detailsresponse);
 					document.getElementById("displayrt").innerHTML = 'Movie Run-Time: ' + timeConvert(movieruntime) + '<br><br>';
 					console.log("movie run time: " + movieruntime);
-					
-					
-					
+
+
+
 				}
 			});
 			var getcast = $.ajax({
 				url: 'https://api.planmymovie.com/3/movie/credits/?id=' + $input.val(),
 				type: 'GET',
 				dataType: 'json',
-				error: function() {
+				error: function () {
 					console();
 				},
-				success: function(castresponse) {
-					
+				success: function (castresponse) {
+
 					console.log(castresponse);
 
 					var uhidk;
-					
-					try {
-					uhidk = castresponse.cast[0].name + " as " + castresponse.cast[0].character + "<br>";
-					
-					for (var i=1; i<5 && i<castresponse.cast.length; i++) {
-						uhidk = uhidk + castresponse.cast[i].name + " as " + castresponse.cast[i].character + "<br>";
 
-					}
-					
-					
+					try {
+						uhidk = castresponse.cast[0].name + " as " + castresponse.cast[0].character + "<br>";
+
+						for (var i = 1; i < 5 && i < castresponse.cast.length; i++) {
+							uhidk = uhidk + castresponse.cast[i].name + " as " + castresponse.cast[i].character + "<br>";
+
+						}
+
+
 					} catch (e) {
 						var uhidk = "No Cast Information Available.";
 					}
-					
 
-					
+
+
 					document.getElementById("selectedmoviecast").innerHTML = uhidk;
-					
-					
+
+
 				}
 			});
-			
+
 		}
 	});
 });
 
-$(function() {
+$(function () {
 	$('#timepickergo').datetimepicker({
 		controlType: 'select',
 		timeFormat: "h:mm TT",
@@ -182,55 +158,62 @@ $(function() {
 	})
 })
 
-function getLocaltime(){
+function getLocaltime() {
 	currenttime = new Date().toLocaleString();
 	result = currenttime.replace(',', '');
 	return result;
 }
 
-function calctimereg(){
-	if (movieruntime===void 0) {
+function calctimereg() {
+	if (movieruntime === void 0) {
 		document.getElementById("calctime").innerHTML = 'Calculated Movie End Time: ' + 'Select a movie and try again!' + '<br><br>';
-	}else{
+	} else {
 		document.getElementById("calctime").innerHTML = 'Calculated Movie End Time: ' + addMinutes(currenttime, movieruntime).toLocaleString() + '<br><br>';
-}}
+	}
+}
 
-function calctimecust(){
+function calctimecust() {
 
 	var custtimevalue = $('#timepickergo').datetimepicker('getDate');
-	
-	if (movieruntime===void 0 && custtimevalue!==null) {
-		document.getElementById("custcalctime").innerHTML = 'Calculated Movie End Time: ' + 'Select a movie and try again!' + '<br><br>';
-		console.log("movie runtime was " + movieruntime + " and entered custom time was " + custtimevalue); 
-	}else if (custtimevalue===null && movieruntime!==void 0){
-		document.getElementById("custcalctime").innerHTML = 'Calculated Movie End Time: ' + 'Select a custom time and try again!' + '<br><br>';
-		console.log("movie runtime was " + movieruntime + " and entered custom time was " + custtimevalue); 
-	}else if (movieruntime===void 0 && custtimevalue===null){
-		document.getElementById("custcalctime").innerHTML = 'Calculated Movie End Time: ' + 'Select a movie and custom time and try again!' + '<br><br>';
-		console.log("movie runtime was " + movieruntime + " and entered custom time was " + custtimevalue); 
-	}else{
-		
-		document.getElementById("custcalctime").innerHTML = 'Calculated Movie End Time: ' + addMinutesCust(custtimevalue, movieruntime).toLocaleString() + '<br><br>';
-}}
 
-var timeConvert = function(n){
-	var minutes = n%60;
+	if (movieruntime === void 0 && custtimevalue !== null) {
+		document.getElementById("custcalctime").innerHTML = 'Calculated Movie End Time: ' + 'Select a movie and try again!' + '<br><br>';
+		console.log("movie runtime was " + movieruntime + " and entered custom time was " + custtimevalue);
+	} else if (custtimevalue === null && movieruntime !== void 0) {
+		document.getElementById("custcalctime").innerHTML = 'Calculated Movie End Time: ' + 'Select a custom time and try again!' + '<br><br>';
+		console.log("movie runtime was " + movieruntime + " and entered custom time was " + custtimevalue);
+	} else if (movieruntime === void 0 && custtimevalue === null) {
+		document.getElementById("custcalctime").innerHTML = 'Calculated Movie End Time: ' + 'Select a movie and custom time and try again!' + '<br><br>';
+		console.log("movie runtime was " + movieruntime + " and entered custom time was " + custtimevalue);
+	} else {
+
+		document.getElementById("custcalctime").innerHTML = 'Calculated Movie End Time: ' + addMinutesCust(custtimevalue, movieruntime).toLocaleString() + '<br><br>';
+	}
+}
+
+var timeConvert = function (n) {
+	var minutes = n % 60;
 	var hours = (n - minutes) / 60;
 	return hours + " hour(s) and " + minutes + " minute(s)";
 }
 
 function addMinutes(date, minutes) {
-	movieendtime = new Date(+date + minutes*60000);
+	movieendtime = new Date(+date + minutes * 60000);
 	console.log("movie end time: " + movieendtime.toLocaleString());
 	return movieendtime;
 }
 
 function addMinutesCust(date, minutes) {
-	moviecustendtime = new Date(+date + minutes*60000);
+	moviecustendtime = new Date(+date + minutes * 60000);
 	console.log("movie end time: " + moviecustendtime.toLocaleString());
 	return moviecustendtime;
 }
 
 function setSystemtime() {
 	document.getElementById("timepickergo").value = getLocaltime();
+}
+
+function copyToclip() {
+	navigator.clipboard.writeText("https://dev.planmymovie.com/?id=" + currentmovieid);
+	console.log(currentmovieid);
 }
