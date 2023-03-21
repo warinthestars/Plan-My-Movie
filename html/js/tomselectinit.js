003743b396d4680f48c7d4487da3f3fdbb0385e4
Stone Frangowlakis <stonefrango+github@gmail.com>
2023-03-20 21:45:47 -0400

    more removal of unused code

--- html/js/tomselectinit.js
+++ html/js/tomselectinit.js
@@ -24,21 +24,6 @@ var settings = {
 		if (!query.length) {
 			return callback();
         }
-	/*	$.ajax({
-			url: 'https://api.planmymovie.com/3/search/movie/',
-			type: 'GET',
-			dataType: 'json',
-			data: {
-				query: query,
-			},
-			error: function (e) {
-				callback(e);
-			},
-			success: function (res) {
-				callback(res.results);
-			}
-		});
-	}, */
     var url = 'https://api.planmymovie.com/3/search/movie/?query=' + encodeURIComponent(query);
     fetch(url)
         .then(response => response.json())
